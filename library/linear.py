95b653c68ff641f2c6e7fcffbfbae90ea5f87c5f
mirkolantieri <mirklan@outlook.it>
2020-05-20 12:14:15 +0200

    Folder library
    Nuovo folder per la libreria standard; inizializzato il file con la classe astratta ;

---
+++ library/linear.py
@@ -0,0 +1,14 @@
+"""
+Il file linear.py rappresenta la libreria generale che si va a costruire
+come struttura generale, per la risoluzione dei metodi lineari.
+Come indicato, servirà da struttura di base, per gli altri metodi, i 
+quali verranno definiti nel folder /library
+"""
+
+# importo delle librerie standard essenziali per la lettura delle matrici
+
+import numpy as np
+import scipy
+
+
+class System:
